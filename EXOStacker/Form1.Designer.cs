662198e3525d1862003b238a32538bf2c4678073
MaverickTse@gmail.com <MaverickTse@gmail.com>
2015-01-17 14:46:03 +0800

    First commit

---
+++ EXOStacker/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace EXOStacker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFilename = new System.Windows.Forms.ListBox();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lnkAbout = new System.Windows.Forms.LinkLabel();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstFilename
+            // 
+            this.lstFilename.AllowDrop = true;
+            this.lstFilename.FormattingEnabled = true;
+            this.lstFilename.HorizontalScrollbar = true;
+            this.lstFilename.ItemHeight = 12;
+            this.lstFilename.Location = new System.Drawing.Point(12, 12);
+            this.lstFilename.Name = "lstFilename";
+            this.lstFilename.Size = new System.Drawing.Size(410, 412);
+            this.lstFilename.TabIndex = 0;
+            this.lstFilename.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstFilename_DragDrop);
+            this.lstFilename.DragEnter += new System.Windows.Forms.DragEventHandler(this.lstFilename_DragEnter);
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(441, 12);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(84, 32);
+            this.btnAddFile.TabIndex = 1;
+            this.btnAddFile.Text = "Add Files";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Location = new System.Drawing.Point(441, 65);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(84, 32);
+            this.btnMoveUp.TabIndex = 2;
+            this.btnMoveUp.Text = "Move Up↑";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Click += new System.EventHandler(this.btnMoveUp_Click);
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Location = new System.Drawing.Point(441, 107);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(84, 32);
+            this.btnMoveDown.TabIndex = 3;
+            this.btnMoveDown.Text = "Move Down↓";
+            this.btnMoveDown.UseVisualStyleBackColor = true;
+            this.btnMoveDown.Click += new System.EventHandler(this.btnMoveDown_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(441, 391);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 32);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lnkAbout
+            // 
+            this.lnkAbout.AutoSize = true;
+            this.lnkAbout.Location = new System.Drawing.Point(15, 440);
+            this.lnkAbout.Name = "lnkAbout";
+            this.lnkAbout.Size = new System.Drawing.Size(235, 12);
+            this.lnkAbout.TabIndex = 5;
+            this.lnkAbout.TabStop = true;
+            this.lnkAbout.Text = "EXO Stacker for AviUtl, by Maverick Tse (2014)";
+            this.lnkAbout.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkAbout_LinkClicked);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(441, 248);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(84, 32);
+            this.btnDel.TabIndex = 6;
+            this.btnDel.Text = "Delete Item";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(441, 292);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 32);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear List";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 467);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.lnkAbout);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnMoveDown);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.lstFilename);
+            this.Name = "Form1";
+            this.Text = "EXO Stacker for AviUtl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFilename;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.LinkLabel lnkAbout;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ EXOStacker/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EXOStacker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void MoveItem(int direction)
+        {
+            if (lstFilename.SelectedItem == null || lstFilename.SelectedIndex < 0) return; //nothing selected
+            int new_idx = lstFilename.SelectedIndex + direction;
+            if(new_idx < 0)
+            {
+                new_idx = lstFilename.Items.Count - 1;
+            }
+            else if(new_idx >= lstFilename.Items.Count)
+            {
+                new_idx = 0;
+            }
+            object selected = lstFilename.SelectedItem;
+            lstFilename.Items.Remove(selected);
+            lstFilename.Items.Insert(new_idx, selected);
+            lstFilename.SetSelected(new_idx, true);
+        }
+
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDlg = new OpenFileDialog();
+            fileDlg.Filter = "AviUtl EXO(*.exo)|*.exo";
+            fileDlg.Multiselect = true;
+            fileDlg.Title = "Select AviUtl EXO files";
+            if(fileDlg.ShowDialog()==DialogResult.OK)
+            {
+                lstFilename.BeginUpdate();
+                foreach(String file in fileDlg.FileNames)
+                {
+                    lstFilename.Items.Add(file);
+                }
+                lstFilename.EndUpdate();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstFilename.Items.Clear();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            lstFilename.Items.Remove(lstFilename.SelectedItem);
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            MoveItem(-1);
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e)
+        {
+            MoveItem(1);
+        }
+
+        private void lstFilename_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else 
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void lstFilename_DragDrop(object sender, DragEventArgs e)
+        {
+            String[] ddlist = (String[])(e.Data.GetData(DataFormats.FileDrop, false));
+            foreach(String file in ddlist)
+            {
+                if(file.Contains(".exo") || file.Contains(".EXO"))
+                {
+                    lstFilename.Items.Add(file);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (lstFilename.Items.Count <= 0) return; //empty list
+            SaveFileDialog sfDlg = new SaveFileDialog();
+            sfDlg.AddExtension = true;
+            sfDlg.DefaultExt = "exo";
+            sfDlg.Filter = "AviUtl EXO(*.exo)|*.exo";
+            sfDlg.ValidateNames = true;
+            sfDlg.Title = "Save merged EXO";
+            if (sfDlg.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            //Process otherwise
+            bool firstfile = true;
+            StringBuilder outbuf = new StringBuilder();
+            int max_obj = 0;
+            int max_layer = 0;
+            int max_perfile_layer = 0;
+            int max_perfile_obj = 0;
+            foreach(String file in lstFilename.Items)
+            {
+                using (FileStream fs = File.Open(file, FileMode.Open))
+                {
+                    using (BufferedStream bs = new BufferedStream(fs))
+                    {
+                        using(StreamReader sr= new StreamReader(bs,Encoding.GetEncoding(932)))//S-JIS
+                        {
+                            string s = String.Empty;
+                            while((s=sr.ReadLine())!= null)
+                            {
+                                bool isFileHead = s.Equals("[exedit]", StringComparison.OrdinalIgnoreCase);
+                                bool isSectionHead = s.StartsWith("[") && s.Contains(']') && !s.Contains('.');
+                                bool isSubSectionHead = s.StartsWith("[") && s.EndsWith("]") && s.Contains('.');
+                                bool layertag = s.StartsWith("layer=", StringComparison.OrdinalIgnoreCase);
+                                bool isWidth, isHeight, isScale, isRate, isAuRate, isAuCh, isLength;
+                                isWidth = s.StartsWith("width=");
+                                isHeight = s.StartsWith("height=");
+                                isRate = s.StartsWith("rate=");
+                                isScale = s.StartsWith("scale=");
+                                isLength = s.StartsWith("length=");
+                                isAuRate = s.StartsWith("audio_rate=");
+                                isAuCh = s.StartsWith("audio_ch=");
+
+                                if(!isFileHead && !isSectionHead && !isSubSectionHead && !layertag && !isWidth && !isHeight && !isRate && !isScale && !isLength && !isAuRate && !isAuCh)
+                                {
+                                    outbuf.AppendLine(s);
+                                }
+                                else if(isFileHead || isWidth || isHeight || isRate || isScale || isLength || isAuRate || isAuCh)
+                                {
+                                    if(firstfile)
+                                    {
+                                        outbuf.AppendLine(s);
+                                    }
+                                    
+                                }
+                                else if(layertag)
+                                {
+                                    int eqsign = s.IndexOf("=");
+                                    String str_no = s.Substring(eqsign + 1);
+                                    int layer_no = Int32.Parse(str_no);
+                                    if(firstfile)
+                                    {
+                                        outbuf.AppendLine(s);
+                                        if(layer_no>max_layer)
+                                        {
+                                            max_layer = layer_no;
+                                            max_perfile_layer = layer_no;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        int this_layer = layer_no + max_layer;
+                                        if(layer_no> max_perfile_layer)
+                                        {
+                                            max_perfile_layer = layer_no;
+                                        }
+                                        outbuf.Append("layer=");
+                                        outbuf.AppendLine(this_layer.ToString());
+                                    }
+                                }
+                                else if(isSectionHead || isSubSectionHead)
+                                {
+                                    int substr_len = 0;
+                                    if(isSectionHead)
+                                    {
+                                        substr_len = s.Length - 2;
+                                    }
+                                    else
+                                    {
+                                        int dotpos = s.IndexOf('.');
+                                        substr_len = dotpos - 1;
+                                    }
+                                    String secnum_str = s.Substring(1, substr_len);
+                                    int section_num = Int32.Parse(secnum_str);
+                                    if(firstfile)
+                                    {
+                                        outbuf.AppendLine(s);
+                                        if(section_num> max_perfile_obj)
+                                        {
+                                            max_perfile_obj = section_num;
+                                            max_obj = max_perfile_obj;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        int this_sec_num = section_num + max_obj;
+                                        if(section_num > max_perfile_obj)
+                                        {
+                                            max_perfile_obj = section_num;
+                                        }
+                                        outbuf.Append("[");
+                                        outbuf.Append(this_sec_num.ToString());
+                                        if(isSubSectionHead)
+                                        {
+                                            outbuf.AppendLine(s.Substring(s.IndexOf('.')));
+                                        }
+                                        else
+                                        {
+                                            outbuf.AppendLine("]");
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                            sr.Close();
+                        }
+                        bs.Close();
+                    }
+                    fs.Close();
+                }
+                if(!firstfile)
+                {
+                    max_layer += max_perfile_layer;
+                    max_obj += max_perfile_obj + 1;
+                }
+                else
+                {
+                    max_obj++;
+                }
+                
+                max_perfile_layer = 0;
+                max_perfile_obj = 0;
+                firstfile = false;
+            }
+            String outfile = sfDlg.FileName;
+            using(FileStream ofs = File.Open(outfile, FileMode.Create))
+            {
+                using(BufferedStream obs= new BufferedStream(ofs))
+                {
+                    using (StreamWriter osw = new StreamWriter(obs, Encoding.GetEncoding(932)))
+                    {
+                        osw.Write(outbuf);
+                        osw.Flush();
+                        osw.Close();
+                    }
+                    ofs.Close();
+                }
+                ofs.Close();
+                MessageBox.Show("EXO Written!", "DONE");
+            }
+            
+        }
+
+        private void lnkAbout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://mavericktse.mooo.com/wordpress/");
+        }
+        
+        
+    }
+}
